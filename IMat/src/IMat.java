201479c50a14c94b5111939409dea22dc5bd5cf1
AliAlladin <ali.alladin20@gmail.com>
2020-05-11 16:01:54 +0200

    Har börjat med layouten i FXML-filen utefter designen.

--- IMat/src/IMat.java
+++ IMat/src/IMat.java
@@ -1,6 +1,32 @@
-public class IMat {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.util.ResourceBundle;
+
+public class IMat extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        ResourceBundle bundle = java.util.ResourceBundle.getBundle("IMat");
+
+        Parent root = FXMLLoader.load(getClass().getResource("iMat.fxml"), bundle);
+
+        Scene scene = new Scene(root, 1920, 1080);
+
+        stage.setTitle(bundle.getString("application.name"));
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        System.out.println("hi");
+        launch(args);
     }
 }
