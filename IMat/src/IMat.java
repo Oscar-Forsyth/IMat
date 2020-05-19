7789f6000217227dde6402441dbfef19a5545b73
Victorcousin <57449953+Victorcousin@users.noreply.github.com>
2020-05-19 16:59:56 +0200

    Add files via upload

--- IMat/src/IMat.java
+++ IMat/src/IMat.java
@@ -7,6 +7,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import se.chalmers.cse.dat216.project.IMatDataHandler;
 
 public class IMat extends Application {
 
@@ -17,13 +19,20 @@ public class IMat extends Application {
 
         Parent root = FXMLLoader.load(getClass().getResource("IMat.fxml"), bundle);
 
+
         Scene scene = new Scene(root, 1980, 1080);
 
         stage.setTitle(bundle.getString("application.name"));
         stage.setScene(scene);
+        stage.setMaximized(true);
+        stage.setOnCloseRequest((WindowEvent event1) -> {
+            IMatDataHandler.getInstance().shutDown();
+        });
+
         stage.show();
 
     }
+
 
     /**
      * @param args the command line arguments
---
+++ IMat/src/MyPagesTextField.java
@@ -0,0 +1,69 @@
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import se.chalmers.cse.dat216.project.IMatDataHandler;
+import se.chalmers.cse.dat216.project.Product;
+
+import java.io.IOException;
+
+public class MyPagesTextField extends AnchorPane {
+    private IMatController parentController;
+    @FXML private TextField prename;
+    @FXML private TextField surname;
+    @FXML private TextField adress;
+    @FXML private TextField email;
+    @FXML private TextField phonenumber;
+    @FXML private TextField postalCode;
+    @FXML private TextField coveredCredit;
+
+    public MyPagesTextField(IMatController iMatController){
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MyPagesTextField.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+        this.parentController = iMatController;
+
+        updateCustomerInfo();
+
+
+    }
+     public void saveCustomerInfo(){
+
+           parentController.setFirstName(prename.getText());
+           parentController.setLastName(surname.getText());
+            parentController.setAddress(adress.getText());
+            parentController.setEmail(email.getText());
+            parentController.setPhoneNumber(phonenumber.getText());
+            parentController.setPostCode(postalCode.getText());
+
+    }
+    public void updateCustomerInfo(){
+        if(parentController.getFirstname()!=null){
+            prename.setText(parentController.getFirstname());
+        }
+        if(parentController.getLastname()!=null){
+            surname.setText(parentController.getLastname());
+        }
+        if(parentController.getAddress()!=null){
+            adress.setText(parentController.getAddress());
+        }
+        if(parentController.getEmail()!=null){
+            email.setText(parentController.getEmail());
+        }
+        if(parentController.getPhoneNumber()!=null){
+            phonenumber.setText(parentController.getPhoneNumber());
+        }
+        if(parentController.getPostCode()!=null){
+            postalCode.setText(parentController.getPostCode());
+        }
+    }
+}
